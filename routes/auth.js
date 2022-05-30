616dc4674181eecd5b48e9c09cf00747034ba45c
Sergio Sanchez <chechoo.alejandro@gmail.com>
2022-05-30 11:24:43 -0500

    env

--- routes/auth.js
+++ routes/auth.js
@@ -16,26 +16,26 @@ const router = Router();
 
 router.post(
   "/new",
-  [
-    check("name", "El nombre es obligatorio").not().isEmpty(),
-    check("email", "El email es obligatorio").isEmail(),
-    check("password", "El password debe de ser de 6 caracteres").isLength({
-      min: 6,
-    }),
-    validarCampos,
-  ],
+  // [
+  //   check("name", "El nombre es obligatorio").not().isEmpty(),
+  //   check("email", "El email es obligatorio").isEmail(),
+  //   check("password", "El password debe de ser de 6 caracteres").isLength({
+  //     min: 6,
+  //   }),
+  //   validarCampos,
+  // ],
   crearUsuario
 );
 
 router.post(
   "/",
-  [
-    check("email", "El email es obligatorio").isEmail(),
-    check("password", "El password debe de ser de 6 caracteres").isLength({
-      min: 6,
-    }),
-    validarCampos,
-  ],
+  // [
+  //   check("email", "El email es obligatorio").isEmail(),
+  //   check("password", "El password debe de ser de 6 caracteres").isLength({
+  //     min: 6,
+  //   }),
+  //   validarCampos,
+  // ],
   loginUsuario
 );
 
